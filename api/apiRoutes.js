441bef630d38a6fd3d2d539dab5e17aa0b694bfc
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2025-04-10 12:55:06 +0530

    api routes file edited

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -3,14 +3,17 @@ const path = require('path')
 const fs = require('fs')
 const router = express.Router()
 
+// Home route
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/home.html'))
+  res.render('home') // Render EJS template instead of sending an HTML file
 })
 
+// Sign-in route (GET)
 router.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname, '/sign-in.html'))
+  res.render('sign-in') // Render EJS template for the sign-in page
 })
 
+// Sign-in route (POST)
 router.post('/signin', (req, res, next) => {
   const { username, password } = req.body
   fs.readFile(path.join(__dirname, '../models/users.json'), 'utf-8', (err, data) => {
@@ -18,17 +21,20 @@ router.post('/signin', (req, res, next) => {
     const users = JSON.parse(data)
     const user = users.find(u => u.username === username && u.password === password)
     if (user) {
-      return res.status(302).redirect('/index.html')
+      // Redirecting to EJS page
+      return res.status(302).redirect('/index') // Use '/index' route (without the .html extension)
     } else {
-      return res.status(302).redirect('/sign-in.html')
+      return res.status(302).redirect('/signin') // Use '/signin' route (without the .html extension)
     }
   })
 })
 
+// Sign-up route (GET)
 router.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/signup.html'))
+  res.render('signup') // Render EJS template for the signup page
 })
 
+// Sign-up route (POST)
 router.post('/signup', (req, res, next) => {
   const { username, password } = req.body
   const newUser = { username, password }
@@ -41,13 +47,14 @@ router.post('/signup', (req, res, next) => {
     users.push(newUser)
     fs.writeFile(path.join(__dirname, '../models/users.json'), JSON.stringify(users, null, 2), (err) => {
       if (err) return next(err)
-      res.status(302).redirect('/sign-in.html')
+      res.status(302).redirect('/signin') // Redirect to EJS sign-in page
     })
   })
 })
 
+// Index route (GET)
 router.get('/api/index', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'))
+  res.render('index') // Render EJS template for the index page
 })
 
 module.exports = router
