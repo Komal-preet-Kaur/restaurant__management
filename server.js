7d673db16eb04e1862a671be4310647d6e331aa1
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2025-04-10 12:49:16 +0530

    Server and index file edited again

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = 8080
 const logger = require('./middlewares/logger')
 const errorHandler = require('./middlewares/errorHandler')
 
-// Set view engine
+// Set view engine to EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -30,34 +30,64 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes using EJS
+
+// Home route
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+// Index route
 app.get('/index', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    title: 'DineDelight',
+    heading: 'Where every flavor tells a story',
+    subheading: 'Come with family & feel the joy of mouthwatering food',
+    steps: [
+      {
+        title: 'Register',
+        image: '/images/step1image.avif',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Explore Restaurants',
+        image: '/images/step2image.avif',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      },
+      {
+        title: 'Book Table',
+        image: '/images/step3image.avif',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      }
+    ]
+  })
 })
 
+// Explore route
 app.get('/explore', (req, res) => {
-  res.render('exploreRest') // rename your exploreRest.html to exploreRest.ejs
+  res.render('exploreRest') // rename exploreRest.html to exploreRest.ejs
 })
 
+// Sign-in route
 app.get('/signin', (req, res) => {
   res.render('sign-in')
 })
 
+// Sign-up route
 app.get('/signup', (req, res) => {
   res.render('signup')
 })
 
+// About us route
 app.get('/about', (req, res) => {
   res.render('aboutus')
 })
 
+// Contact us route
 app.get('/contact', (req, res) => {
   res.render('contactus')
 })
 
+// Privacy policy route
 app.get('/privacy', (req, res) => {
   res.render('privacypolicy')
 })
@@ -72,8 +102,10 @@ app.use((req, res, next) => {
   next()
 })
 
+// Error handler middleware
 app.use(errorHandler)
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
 })
