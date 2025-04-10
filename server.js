498925235070c5f4d0f08b155d294e0ae2062a06
Komal-preet-Kaur <komalpreetkaur18278@gmail.com>
2025-04-10 12:24:43 +0530

    Partial folder added and server file edited

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const helmet = require('helmet')
 const compression = require('compression')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+
 const app = express()
 const PORT = 8080
 
 const logger = require('./middlewares/logger')
 const errorHandler = require('./middlewares/errorHandler')
 
+// Set view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger)
@@ -19,38 +25,48 @@ app.use(helmet())
 app.use(compression())
 app.use(morgan('dev'))
 app.use(cookieParser())
+
+// Serve static files (like css, js, images) from 'public' folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes using EJS
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'))
+  res.render('home')
 })
-app.get('/explore', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'exploreRest.html'))
-})
+
 app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index')
 })
+
+app.get('/explore', (req, res) => {
+  res.render('exploreRest') // rename your exploreRest.html to exploreRest.ejs
+})
+
 app.get('/signin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'sign-in.html'))
+  res.render('sign-in')
 })
+
 app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'signup.html'))
+  res.render('signup')
 })
+
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'aboutus.html'))
+  res.render('aboutus')
 })
+
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contactus.html'))
+  res.render('contactus')
 })
+
 app.get('/privacy', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'privacypolicy.html'))
+  res.render('privacypolicy')
 })
 
-app.use(express.static(path.join(__dirname, 'views')))
-
+// API Routes
 const apiRoutes = require('./api/apiRoutes')
 app.use('/api', apiRoutes)
 
+// 404 route
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found' })
   next()
